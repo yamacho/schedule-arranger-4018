324d73cc363ee239877d760e67c32538013392b8
yamacho <yamacho@users.noreply.github.com>
2020-03-14 16:53:16 +0900

    予定作成フォームを作成

---
+++ routes/schedules.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const authenticationEnsurer = require('./authentication-ensurer');
+
+router.get('/new', authenticationEnsurer, (req, res, next) => {
+  res.render('new', { user: req.user });
+});
+
+router.post('/', authenticationEnsurer, (req, res, next) => {
+  console.log(req.body); // TODO 予定と候補を保存
+  res.redirect('/');
+});
+
+module.exports = router;
